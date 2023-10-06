8452cf271020be9e4a82c4ee19419cd8b7689f17
PriyanKishoreMS <priyankishore2000@gmail.com>
2023-10-06 16:40:32 +0530

    test commit 5

--- scripts.sh
+++ scripts.sh
@@ -10,13 +10,26 @@
 # fi
 
 
-SERVICE1_CHANGED=$(if git diff --quiet 014120375419ff3ac63a75a3fdbd8718680dcc91 a2e18ba596c6f61dff420b53c613df98a562976a -- 'serverless-start/'; then echo true; else echo false; fi)
-    echo "SERVICE1_CHANGED=${SERVICE1_CHANGED}" 
+# SERVICE1_CHANGED=$(if git diff --quiet 014120375419ff3ac63a75a3fdbd8718680dcc91 a2e18ba596c6f61dff420b53c613df98a562976a -- 'serverless-start/'; then echo true; else echo false; fi)
+#     echo "SERVICE1_CHANGED=${SERVICE1_CHANGED}" 
 
-git diff a2e18ba596c6f61dff420b53c613df98a562976a dcb9ee1d81e4cc670a33026931945c20feefeca4 -- 'serverless-start/' 
+# git diff a2e18ba596c6f61dff420b53c613df98a562976a dcb9ee1d81e4cc670a33026931945c20feefeca4 -- 'serverless-start/' 
 
-if git diff --exit-code a2e18ba596c6f61dff420b53c613df98a562976a dcb9ee1d81e4cc670a33026931945c20feefeca4 -- 'serverless-start/'; then
-  echo "No changes"
-else
-  echo "Changes detected"
-fi
+# if git diff --quiet --exit-code a2e18ba596c6f61dff420b53c613df98a562976a dcb9ee1d81e4cc670a33026931945c20feefeca4 -- 'serverless-start/'; then
+#   echo "No changes"
+# else
+#   echo "Changes detected"
+# fi
+
+# SERVICE1_CHANGED=$(if git diff --quiet --exit-code ${{ github.sha }}^1 ${{ github.sha }} -- 'serverless-start/'; then
+#   echo false
+# else
+#   echo true
+# fi)
+# echo "SERVICE1_CHANGED=${SERVICE1_CHANGED}"
+
+SERVICE1_CHANGED=$(if git diff --quiet --exit-code dcb9ee1d81e4cc670a33026931945c20feefeca4 86db23eed81b53dde2f66abc1a9bc3408f80fc6a -- 'serverless-start/'; then echo false else echo true fi);echo "SERVICE1_CHANGED=${SERVICE1_CHANGED}"
+
+# 86db23eed81b53dde2f66abc1a9bc3408f80fc6a
+# dcb9ee1d81e4cc670a33026931945c20feefeca4
+# a2e18ba596c6f61dff420b53c613df98a562976a
