adbe43b62d29b49f5f2a8f241f2ec3904b488d5d
PriyanKishoreMS <priyankishore2000@gmail.com>
2023-10-06 14:40:38 +0530

    test conditional

--- serverless-extra/index.js
+++ serverless-extra/index.js
@@ -3,8 +3,7 @@ module.exports.handler = async event => {
 		statusCode: 200,
 		body: JSON.stringify(
 			{
-				message:
-					"Go Serverless v3.0! Your function executed successfully extra!",
+				message: "This is the now modified message",
 				input: event,
 			},
 			null,
