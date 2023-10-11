29231c1db17095706bcdfc53481c5f89dfae70fa
PriyanKishoreMS <priyankishore2000@gmail.com>
2023-10-11 16:14:51 +0530

    verify working pre commit

--- serverless-start/index.js
+++ serverless-start/index.js
@@ -14,6 +14,12 @@ app.get("/path", (req, res, next) => {
 	});
 });
 
+app.get("/check", (req, res, next) => {
+	return res.status(200).json({
+		message: "Hello from check path!",
+	});
+});
+
 app.use((req, res, next) => {
 	return res.status(404).json({
 		error: "Not Found",
