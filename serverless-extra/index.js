92b600d2fef031aa8c85648a744b5b5b978a93ed
PriyanKishoreMS <priyankishore2000@gmail.com>
2023-10-12 11:53:34 +0530

    matrix 2

--- serverless-extra/index.js
+++ serverless-extra/index.js
@@ -3,7 +3,7 @@ module.exports.handler = async event => {
 		statusCode: 200,
 		body: JSON.stringify(
 			{
-				message: "This is the modular message",
+				message: "This is the matrix talking",
 				input: event,
 			},
 			null,
