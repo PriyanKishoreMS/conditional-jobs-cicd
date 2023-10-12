c491ce6d2f305cf5a29c7966fca0ccfe28b8f7e5
PriyanKishoreMS <priyankishore2000@gmail.com>
2023-10-12 17:31:12 +0530

    testing 3

--- serverless-extra/index.js
+++ serverless-extra/index.js
@@ -3,7 +3,7 @@ module.exports.handler = async event => {
 		statusCode: 200,
 		body: JSON.stringify(
 			{
-				message: "This is now working",
+				message: "This is now working 2",
 				input: event,
 			},
 			null,
